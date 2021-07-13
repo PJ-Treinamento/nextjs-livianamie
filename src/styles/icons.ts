afbe96e70393ec2194cecda32c9b102643be33ae
Lívia <livianamiesn@gmail.com>
2021-07-13 19:58:43 -0300

    add: added the necessary libs and icons

---
+++ src/styles/icons.ts
@@ -0,0 +1,20 @@
+export {
+    Search,
+    Bell,
+    House,
+    Gear,
+    Envelope,
+    List,
+    Chat,
+    ArrowRepeat,
+    Heart,
+    HeartFill,
+    BookmarkStar,
+    Star,
+    StarFill,
+    Share,
+    Trash,
+    ThreeDots,
+    XCircle
+} from '@styled-icons/bootstrap';
+export { Earlybirds } from '@styled-icons/fa-brands';
