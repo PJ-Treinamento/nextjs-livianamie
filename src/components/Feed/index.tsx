4afca1fb7959eb894cccb83625946ce242d6ca91
Lívia <livianamiesn@gmail.com>
2021-07-14 23:24:18 -0300

    add: made the feed component

---
+++ src/components/Feed/index.tsx
@@ -0,0 +1,14 @@
+import { Main } from './styles';
+import NewPiu from '../NewPiu';
+import Timeline from '../Timeline';
+
+const Feed: React.FC = () => {
+    return (
+        <Main>
+            <NewPiu />
+            <Timeline />
+        </Main>
+    );
+};
+
+export default Feed;
---
+++ src/components/Feed/styles.ts
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export const Main = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+
+    width: 100%;
+
+    margin: 2rem;
+
+    @media (min-width: 500px) {
+        margin-right: 7rem;
+    }
+    @media (min-width: 800px) {
+        margin: 3rem;
+        margin-right: 24rem;
+    }
+    @media (min-width: 1200px) {
+        margin: 4rem;
+        margin-right: 28rem;
+    }
+`;
