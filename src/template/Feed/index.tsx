dc6204e7e80752624fe3521b5d34a54f4bf2a9ee
Lívia <livianamiesn@gmail.com>
2021-07-15 00:30:51 -0300

    feat: made the design of the feed page

---
+++ src/template/Feed/index.tsx
@@ -0,0 +1,23 @@
+import Header from '../../components/Header';
+import Feed from '../../components/Feed';
+import BottomMenu from '../../components/BottomMenu';
+import SideMenu from '../../components/SideMenu';
+
+import { Container, Wrapper, MiddleWrapper } from './styles';
+
+const FeedTemplate: React.FC = () => {
+    return (
+        <Container>
+            <Wrapper>
+                <Header />
+                <MiddleWrapper>
+                    <Feed />
+                    <SideMenu />
+                </MiddleWrapper>
+                <BottomMenu />
+            </Wrapper>
+        </Container>
+    );
+};
+
+export default FeedTemplate;
---
+++ src/template/Feed/styles.ts
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    background: var(--lighter-purple);
+`;
+export const Wrapper = styled.div`
+    width: 100%;
+    height: 100%;
+
+    margin: 0 auto;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+
+    background: url(/background.svg) no-repeat center center fixed;
+    background-size: cover;
+`;
+export const MiddleWrapper = styled.div`
+    width: 100%;
+    height: 100%;
+
+    display: flex;
+    justify-content: space-between;
+`;
