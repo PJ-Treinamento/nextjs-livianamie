413429596ae6beedc528e28b262d016274cb64e4
Lívia <livianamiesn@gmail.com>
2021-07-16 23:12:12 -0300

    refactor: remade the useAuth hook

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -1,33 +1,23 @@
 import { useCallback, useContext, createContext, useState } from 'react';
-import { setCookie } from 'nookies';
+import { destroyCookie, setCookie } from 'nookies';
 import { useRouter } from 'next/router';
-import { IUser } from '../models/index';
 import api from '../services/api';
 
-interface LoginData {
+type LoginData = {
     email: string;
     password: string;
-}
+};
 
-interface AuthState {
-    user: IUser;
-    token: string;
-}
-
-interface AuthContextData {
-    user: IUser;
-    token: string;
+type AuthContextData = {
     error: boolean;
-    loading: boolean;
     login(user: LoginData): void;
-}
+    logout(): void;
+};
 
 const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 
 export const AuthProvider: React.FC = ({ children }) => {
     const [error, setError] = useState(false);
-    const [userData, setUserData] = useState<AuthState>({} as AuthState);
-    const [loading, setLoading] = useState(false);
     const router = useRouter();
 
     const login = useCallback(
@@ -36,7 +26,6 @@ export const AuthProvider: React.FC = ({ children }) => {
                 const response = await api.post('/sessions/login', data);
 
                 const { token } = response.data;
-                const { user } = response.data;
                 const { username } = response.data.user;
 
                 if (token) {
@@ -48,28 +37,28 @@ export const AuthProvider: React.FC = ({ children }) => {
                     setCookie(undefined, '@Piupiuwer:username', username, {
                         maxAge: 60 * 60 * 1
                     });
-
-                    setUserData({ token, user });
                     setError(false);
                     router.push('/feed');
                 }
-                setLoading(false);
             } catch {
                 setError(true);
-                setLoading(false);
             }
         },
         [router]
     );
 
+    const logout = () => {
+        destroyCookie(undefined, '@Piupiuwer:token');
+        destroyCookie(undefined, '@Piupiuwer:username');
+        router.push('/login');
+    };
+
     return (
         <AuthContext.Provider
             value={{
-                user: userData.user,
-                token: userData.token,
                 error,
-                loading,
-                login
+                login,
+                logout
             }}
         >
             {children}
